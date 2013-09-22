ff50538430f29f2b928bd5de88ba16f748004b15
Alexander Pitkin <C:\Documents and Settings\Alexander\Application Data\The Bat!>
2013-09-22 23:45:09 +0300

    Added !quit command
    Some changes in README
    Added !quit command to close connection
    Added port value check

--- lab2/main.py
+++ lab2/main.py
@@ -9,8 +9,12 @@ BUFFER_LENGTH = 1024
 
 
 def run_echo_server(port):
-    '''Run echo server on specified port'''
+    '''Run echo server on specified port
+       raise ValueError if port not in range 0-65535
+    '''
     try:
+        if not 0 <= port <= 65535:
+            raise ValueError("port must be 0-65535")
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(('', port))
         s.listen(1)
@@ -22,6 +26,11 @@ def run_echo_server(port):
                 conn.send(buff)
                 print(buff, end='')
             else:
+                break
+            if buff.strip() == "!quit":
+                print("\nShutdown command recognized")
+                conn.send("Shutdown")
+                conn.shutdown(socket.SHUT_RDWR)
                 break
         print("\nClient %s:%s - disconnected" % addr_info)
         conn.close()
