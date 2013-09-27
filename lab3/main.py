a9b0cf194abc56f10dd0b06d992bfdcdcd815d78
Alexander Pitkin <peleccom@gmail.com>
2013-09-27 11:06:55 +0300

    Added lab3 folder

---
+++ lab3/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+import sys
+import socket
+
+
+BUFFER_LENGTH = 1024
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
